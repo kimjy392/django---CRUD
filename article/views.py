f9b96d72137bbe03311014402af71ca455eea9b2
kimjunyoung <kimjy392@gmail.com>
2019-08-19 16:40:08 +0900

    04 | article - CRUD - C

--- article/views.py
+++ article/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
 
 # Create your views here.
+def index(request):
+    articles = Article.objects.order_by('-id') # 최신글로 정렬하기
+    context = {
+        'articles': articles
+    }
+    return render(request, 'article/index.html', context)
+
+def new(request):
+    return render(request, 'article/new.html')
+
+def create(request):
+    # 저장 로직
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    article = Article.objects.create(title=title, content=content)
+    context = {
+        'article' : article
+    }
+    # return render(request, 'article/create.html', context)
+    return redirect('/article/')
